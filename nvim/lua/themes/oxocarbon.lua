0cfd67d34e728f2c7274f6a7fe9e144e531dd476
Luis Saturno <saturno.luis@gmail.com>
2025-01-05 00:34:10 +0100

    Add amazing oxocarbon colorscheme

---
+++ nvim/lua/themes/oxocarbon.lua
@@ -0,0 +1,6 @@
+-- A dark and light Neovim theme written in fennel, inspired by IBM Carbon.
+-- https://github.com/nyoom-engineering/oxocarbon.nvim
+return {
+	"nyoom-engineering/oxocarbon.nvim",
+	priority = 1000,
+}
