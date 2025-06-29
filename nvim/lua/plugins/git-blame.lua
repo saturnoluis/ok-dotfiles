7ebd78ddbccfec2fb6b3cd009bf9e5a468b2d9a1
Luis Saturno <saturno.luis@gmail.com>
2025-06-29 18:22:37 +0200

    Install git tools plugins - git-blame and gitsigns

---
+++ nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,7 @@
+-- A git blame plugin for Neovim written in Lua
+-- https://github.com/f-person/git-blame.nvim
+
+return {
+	"f-person/git-blame.nvim",
+	opts = {},
+}
---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,7 @@
+-- Super fast git decorations implemented purely in Lua.
+-- https://github.com/lewis6991/gitsigns.nvim
+
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {},
+}
