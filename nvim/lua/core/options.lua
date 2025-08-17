cdbdee3881e8b699346fc6957453a220eb08711e
Luis Saturno <saturno.luis@gmail.com>
2025-08-17 10:50:48 +0200

    improvements

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,7 +3,7 @@ vim.cmd.colorscheme "kanagawa"
 
 -- UI & Visuals
 vim.opt.colorcolumn = "+1"        -- Highlight column after textwidth
-vim.opt.cursorline = false         -- Highlight the current line
+vim.opt.cursorline = false        -- Highlight the current line
 vim.opt.list = true               -- Show special characters
 vim.opt.number = true             -- Show line numbers
 vim.opt.relativenumber = true     -- Show relative line numbers
@@ -12,12 +12,13 @@ vim.opt.wrap = false              -- Disable line wrapping
 
 -- Indentation & Tabs
 vim.opt.expandtab = false         -- Use tabs instead of spaces
-vim.opt.tabstop = 4               -- Tab character is 4 spaces
-vim.opt.softtabstop = 4           -- Tab key inserts 4 spaces
 vim.opt.shiftwidth = 4            -- Indentation size
 vim.opt.smartindent = true        -- Smart auto-indentation
+vim.opt.softtabstop = 4           -- Tab key inserts 4 spaces
+vim.opt.tabstop = 4               -- Tab character is 4 spaces
 
 -- Text & Editing
-vim.opt.textwidth = 80            -- Set max line width to 80
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-vim.o.undofile = true             -- Persistent undo history
+vim.opt.swapfile = false          -- Disable swap files
+vim.opt.textwidth = 80            -- Set max line width to 80
+vim.opt.undofile = true           -- Persistent undo history
