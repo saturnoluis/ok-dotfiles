3e3ecfadafbee01f295267641b9c3afe1356bd3a
Luis Saturno <saturno.luis@gmail.com>
2025-11-01 14:12:39 +0100

    Add HTML, CSS, JS LSP (I think...)

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -9,6 +9,21 @@
 -- The command used by the LSP config matches the server found in Mason.
 -- ===================================================================================
 
+-- CSS
+-- Server: css-lsp (vscode-css-language-server)
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/cssls.lua
+vim.lsp.enable("cssls");
+
+-- HTML
+-- Server: html-lsp (vscode-html-language-server)
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/html.lua
+vim.lsp.enable("html");
+
+-- JavaScript
+-- Server: eslint-lsp  (vscode-eslint-language-server)
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/eslint.lua
+vim.lsp.enable("eslint");
+
 -- Lua lang
 -- Server: lua-language-server
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
