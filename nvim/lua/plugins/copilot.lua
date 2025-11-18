833751f6a13cf3e62a0ad76f8595541f459a8b48
Luis Saturno <saturno.luis@gmail.com>
2025-11-18 13:16:55 +0100

    Add copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+-- ===================================================================================
+-- Plugin: copilot
+-- About:  Copilot is an AI pair programmer tool that helps you write code.
+-- Source: https://github.com/github/copilot.vim
+-- ===================================================================================
+
+return {
+	"github/copilot.vim",
+}
