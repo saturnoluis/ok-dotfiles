248fc7d09661b4aef7c141842ea39be2e2319ba9
Luis Saturno <saturno.luis@gmail.com>
2026-01-17 21:39:47 +0100

    Replace my config with lazy.nvim

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
--- ===================================================================================
--- Plugin: copilot
--- About:  Copilot is an AI pair programmer tool that helps you write code.
--- Source: https://github.com/github/copilot.vim
--- ===================================================================================
-
-return {
-	"github/copilot.vim",
-}
