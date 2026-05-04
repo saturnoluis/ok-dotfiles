e37dac19b5f724835eb6ea3e94a5aa06716bfec1
Luis Saturno <saturno.luis@gmail.com>
2026-05-04 19:22:24 +0200

    Back to lazy (#2)
    I had to move back to lazyvim distro because I was having issues with my own config.
    
    * Replace my config with lazy.nvim
    * use onedark as theme
    * update tmux config
    * add cutlass
    * add tmux popup commands
    * disable formatter plugin stevearc/conform.nvim
    * disable autoformat
    * Add git blame
    * Remove unused kitty themes
    * Remove custom tmux color
    * remove nvim colorscheme background

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,17 +1,8 @@
--- ===================================================================================
--- Custom NeoVim AutoCommands
--- ===================================================================================
-
--- Highlight the yanked text for 200ms
-local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
-vim.api.nvim_create_autocmd("TextYankPost", {
-	group = highlight_yank_group,
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 200,
-		})
-	end,
-})
-
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
