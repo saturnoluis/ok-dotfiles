4ef301d4f8b67db677109c6f34b04c35ae8655ee
Luis Saturno <saturno.luis@gmail.com>
2026-08-30 23:31:04 +0200

    fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,39 +5,40 @@
 -- ===================================================================================
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
-	lazy = false,
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"cpp",
-			"css",
-			"dockerfile",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"lua",
-			"markdown",
-			"python",
-			"scss",
-			"svelte",
-			"typescript",
-			"vim",
-			"vue",
-			"yaml",
-		},
-		auto_install = true,
-		sync_install = false,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlight = false,
-		},
-		indent = {
-			enable = true,
-		};
-	},
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+        local parsers = {
+            "bash",
+            "c",
+            "cpp",
+            "css",
+            "dockerfile",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "lua",
+            "markdown",
+            "python",
+            "scss",
+            "svelte",
+            "typescript",
+            "vim",
+            "vue",
+            "yaml",
+        }
+
+        -- Install parsers
+        require("nvim-treesitter").install(parsers)
+
+        -- Enable features for installed filetypes
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
