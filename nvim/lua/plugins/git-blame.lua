aeada2b607b4920cc644c005866b0bdb93487747
Luis Saturno <saturno.luis@gmail.com>
2026-09-01 17:07:18 +0200

    Add flash, git-blame, blankline

---
+++ nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,10 @@
+-- ===================================================================================
+-- Plugin: git-blame
+-- About:  A git blame plugin for Neovim written in Lua
+-- Source: https://github.com/f-person/git-blame.nvim
+-- ===================================================================================
+
+return {
+	"f-person/git-blame.nvim",
+	opts = {},
+}
